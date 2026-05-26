da8f462d72e6007c931cb99abf97275efe987662
Harley Driscoll <79179990+FiredAxeman@users.noreply.github.com>
2026-05-26 14:24:35 -0500

    Added Chapter 7 Disc

--- Chapter6/CT6Disc.java
+++ Chapter6/CT6Disc.java
@@ -1,13 +1,38 @@
 import java.io.Serializable;
 import java.util.logging.Logger;
 
+public class CT6Disc {
+    public static void main(String[] args) {
+        System.out.println("--- 1. Application Initialization ---");
+        
+        // This instantiation triggers the noncompliant logger and toString() leak
+        UserProfile user = new UserProfile(
+            "jdoe88", 
+            "SuperSecretPassword123!", 
+            "4111-2222-3333-4444"
+        );
+        
+        System.out.println("\n--- 2. Normal Processing ---");
+        // Simulating a successful transaction
+        user.processPayment(150.00);
+        
+        System.out.println("\n--- 3. Error Handling ---");
+        // Simulating an error state that leaks data into an exception
+        try {
+            user.processPayment(-10.00);
+        } catch (IllegalArgumentException e) {
+            System.err.println("EXCEPTION CAUGHT: " + e.getMessage());
+        }
+    }
+}
+
 // NONCOMPLIANT: Implements Serializable without protecting sensitive fields
-public class UserProfile implements Serializable {
+class UserProfile implements Serializable {
     private static final long serialVersionUID = 1L;
     private static final Logger logger = Logger.getLogger(UserProfile.class.getName());
 
     private String username;
-
+    
     // NONCOMPLIANT: Storing passwords and credit cards as immutable Strings
     private String password;
     private String creditCardNumber;
@@ -16,7 +41,7 @@ public class UserProfile implements Serializable {
         this.username = username;
         this.password = password;
         this.creditCardNumber = creditCardNumber;
-
+        
         // NONCOMPLIANT: Logging sensitive data upon creation
         logger.info("New UserProfile created: " + this.toString());
     }
